103879ddad47bc1b6408ccc379126bb62a8b5aa3
Jake Perez <jacobperez@berkeley.edu>
2020-04-07 01:44:24 -0500

    Set coverage thresholds  - Todo: Tighten and enforce

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,15 @@ module.exports = {
     "^.+\\.ts?$": "ts-jest"
   },
   "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$",
+  "collectCoverageFrom": [
+    "**/*.ts",
+  ],
+  "coverageThreshold": {
+    "global": {
+      "branches": 100,
+      "functions": 100,
+      "lines": 100,
+      "statements": 100,
+    },
+  }
 }
