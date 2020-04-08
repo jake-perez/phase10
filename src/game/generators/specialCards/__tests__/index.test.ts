8785786fd2df4abf116c03de0595716edef5c129
Jake Perez <jacobperez@berkeley.edu>
2020-04-08 00:40:45 -0500

    Update generators for special cards

--- src/game/generators/specialCards/__tests__/index.test.ts
+++ src/game/generators/specialCards/__tests__/index.test.ts
@@ -3,13 +3,31 @@ import { Card } from '../../../interfaces/Card';
 
 describe('Special Cards', () => {
   it('should make sure wildcard has correct values', () => {
-    const wildcard: Card = WildCard;
+    const wildCard: Card = WildCard;
 
-    expect(wildcard).toEqual({
+    expect(wildCard).toEqual({
       color: null,
       value: null,
       lossValue: 25,
       type: 'WILD',
     });
+  });
+
+  it('should make sure skipcards has correct values', () => {
+    const skipCard: Card = SkipCard;
+
+    expect(skipCard).toEqual({
+      color: null,
+      value: null,
+      lossValue: 15,
+      type: 'SKIP',
+    });
+  });
+
+  it('should have 8 wilds and 4 skips', () => {
+    const specialCards: Card[] = SPECIAL_CARDS;
+
+    expect(specialCards.filter((e) => e.type === 'WILD')).toHaveLength(8);
+    expect(specialCards.filter((e) => e.type === 'SKIP')).toHaveLength(4);
   });
 });
